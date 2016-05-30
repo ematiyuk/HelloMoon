f4d3ff457d6be46dfe64c0cd0d69369daaae1190
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-30 17:38:53 +0300

    Add onCompletion playback listener
    - Add an initial call to stop()
    - Set a listener to call stop() when the audio has finished playing

--- android/src/main/java/com/bignerdranch/android/hellomoon/AudioPlayer.java
+++ android/src/main/java/com/bignerdranch/android/hellomoon/AudioPlayer.java
@@ -14,7 +14,21 @@ public class AudioPlayer {
     }
 
     public void play(Context context) {
+        /* it prevents the creation of multiple instances of MediaPlayer
+           if the user clicks the Play button twice */
+        stop();
+
         mPlayer = MediaPlayer.create(context, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                /* Calling stop() when the file has finished playing releases your hold
+           on the MediaPlayer instance as soon as you no longer need it */
+                stop();
+            }
+        });
+
         mPlayer.start();
     }
 }
