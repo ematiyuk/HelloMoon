09faafeb4e87d03108099ab1251e0db3b727cfc2
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-31 11:38:12 +0300

    Add HelloMoonVideo activity and fragment

---
+++ android/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonVideoActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.hellomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HelloMoonVideoActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon_video);
+    }
+}
